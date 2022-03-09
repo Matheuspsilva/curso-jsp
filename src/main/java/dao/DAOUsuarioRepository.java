7477d6a8987f7c66b06e331e3790e7ffb9d1381f
Matheus <matheuspsilva29@gmail.com>
2022-03-09 17:37:49 -0300

    Add Consultar usuário

--- src/main/java/dao/DAOUsuarioRepository.java
+++ src/main/java/dao/DAOUsuarioRepository.java
@@ -2,6 +2,7 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import connection.SingleConnectionBanco;
@@ -15,7 +16,7 @@ public class DAOUsuarioRepository {
 		connection = SingleConnectionBanco.getConnection();
 	}
 
-	public void gravarUsuario(ModelLogin objeto) throws Exception {
+	public ModelLogin gravarUsuario(ModelLogin objeto) throws Exception {
 
 			String sql = "INSERT INTO model_login( login, senha, nome, email) VALUES (?, ?, ?, ?);";
 
@@ -28,7 +29,33 @@ public class DAOUsuarioRepository {
 			
 			preparedStatement.execute();
 			connection.commit();
+			
+			return this.consultarUsuario(objeto.getLogin());
 
+	}
+	
+	public ModelLogin consultarUsuario(String login) throws Exception {
+		
+		ModelLogin modelLogin = new ModelLogin();
+		
+		String sql = "SELECT * FROM model_login WHERE login = ? ;";
+		
+		PreparedStatement preparedStatement = connection.prepareStatement(sql);
+		preparedStatement.setString(1, login);
+		
+		ResultSet resultado = preparedStatement.executeQuery();
+		
+		while (resultado.next()) { // Retorna apenas um usuário
+
+			modelLogin.setId(resultado.getLong("id"));
+			modelLogin.setNome(resultado.getString("nome"));
+			modelLogin.setLogin(resultado.getString("login"));
+			modelLogin.setEmail(resultado.getString("email"));
+			modelLogin.setSenha(resultado.getString("senha"));
+
+		}
+		
+		return modelLogin;
 	}
 
 }
